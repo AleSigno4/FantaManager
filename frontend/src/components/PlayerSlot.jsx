1953f3ce61cf0beb83ad5b629d0d2b00eca2a203
Alessandro Signori <alesignori4@gmail.com>
2026-07-24 11:12:29 +0200

    feat(PlayerSlot.jsx): Create clickable card player

---
+++ frontend/src/components/PlayerSlot.jsx
@@ -0,0 +1,45 @@
+import { roleColorsMantra } from "../utils/roles";
+
+export default function PlayerSlot({ slot, onSlotClick }) {
+  return (
+    <button
+      className="flex flex-col aspect-13/14 items-center justify-center bg-white/40 rounded-xl border-2 border-black text-center cursor-pointer"
+      onClick={onSlotClick}
+    >
+      {slot?.player ? (
+        <>
+          <div className="flex flex-row pt-1.5 h-5/9">
+            <div className="flex flex-col">
+              <span
+                className={`rounded-full w-6 h-6 inline-flex items-center justify-center font-semibold text-sm 
+                                        ${roleColorsMantra[slot.player.role][0]}`}
+              >
+                {slot.player.role}
+              </span>
+            </div>
+            <img
+              src="assets/lautaro.png"
+              alt="lautaro icon"
+              className="block mx-auto h-full object-contain"
+            />
+          </div>
+          <h3 className="text-gray-100 font-bold">{slot.player.last_name}</h3>
+          <h4 className="text-gray-100 font-semibold text-sm">
+            Inter - Verona
+          </h4>
+          <div className="flex flex-row w-2/3 gap-2 items-center">
+            <h5 className="text-gray-100 text-xs">70%</h5>
+            <div className="border bg-gray-100 rounded-2xl w-2/3 h-1 overflow-hidden">
+              <div
+                className="bg-green-600 h-full"
+                style={{ width: "70%" }}
+              ></div>
+            </div>
+          </div>
+        </>
+      ) : (
+        <p className="text-5xl font-bold text-gray-200">+</p>
+      )}
+    </button>
+  );
+}
