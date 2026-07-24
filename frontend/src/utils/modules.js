8e550b739920c091ee6c15511fbb503154c8766a
Alessandro Signori <alesignori4@gmail.com>
2026-07-24 10:09:52 +0200

    feat(modules.js):Create available  modules arrays

---
+++ frontend/src/utils/modules.js
@@ -0,0 +1,23 @@
+export const classicModules = {
+  "3-4-3": ["Por", "Def", "Def", "Def", "Cen", "Cen", "Cen", "Cen", "Att", "Att", "Att"],
+  "3-5-2": ["Por", "Def", "Def", "Def", "Cen", "Cen", "Cen", "Cen", "Cen", "Att", "Att"],
+  "4-3-3": ["Por", "Def", "Def", "Def", "Def", "Cen", "Cen", "Cen", "Att", "Att", "Att"],
+  "4-4-2": ["Por", "Def", "Def", "Def", "Def", "Cen", "Cen", "Cen", "Cen", "Att", "Att"],
+  "4-5-1": ["Por", "Def", "Def", "Def", "Def", "Cen", "Cen", "Cen", "Cen", "Cen", "Att"],
+  "5-3-2": ["Por", "Def", "Def", "Def", "Def", "Def", "Cen", "Cen", "Cen", "Att", "Att"],
+  "5-4-1": ["Por", "Def", "Def", "Def", "Def", "Def", "Cen", "Cen", "Cen", "Cen", "Att"]
+};
+
+export const mantraModules = {
+  "3-4-3": ["Por", "Dc", "Dc", "Dc/B", "E", "M/C", "C", "E", "W/A", "A/Pc", "W/A"],
+  "3-4-1-2": ["Por", "Dc", "Dc", "Dc/B", "E", "M/C", "C", "E", "T", "A/Pc", "A/Pc"],
+  "3-4-2-1": ["Por", "Dc", "Dc", "Dc/B", "E", "M/C", "M", "E/W", "T", "T/A", "A/Pc"],
+  "3-5-2": ["Por", "Dc", "Dc", "Dc/B", "E", "C", "M", "M/C", "E/W", "A/Pc", "A/Pc"],
+  "3-5-1-1": ["Por", "Dc", "Dc", "Dc/B", "M", "C", "M", "E/W", "E/W", "T/A", "A/Pc"],
+  "4-3-3": ["Por", "Dd", "Dc", "Dc", "Ds", "M/C", "M", "C", "W/A", "A/Pc", "W/A"],
+  "4-3-1-2": ["Por", "Dd", "Dc", "Dc", "Ds", "M/C", "M", "C", "T", "T/A/Pc", "A/Pc"],
+  "4-4-2": ["Por", "Dd", "Dc", "Dc", "Ds", "E", "C", "M/C", "E/W", "A/Pc", "A/Pc"],
+  "4-1-4-1": ["Por", "Dd", "Dc", "Dc", "Ds", "M", "C/T", "T", "E/W", "W", "A/Pc"],
+  "4-4-1-1": ["Por", "Dd", "Dc", "Dc", "Ds", "M", "C", "E/W", "E/W", "T/A", "A/Pc"],
+  "4-2-3-1": ["Por", "Dd", "Dc", "Dc", "Ds", "M", "M/C", "W/T", "T", "W/A", "A/Pc"]
+};
