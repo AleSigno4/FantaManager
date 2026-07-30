44747c0a2e6bc2ad04367aebd5789dc35b91711a
Alessandro Signori <alesignori4@gmail.com>
2026-07-30 11:30:26 +0200

    feat(Leaderboard.jsx): Create general standings with mocks

--- frontend/src/pages/Leaderboard.jsx
+++ frontend/src/pages/Leaderboard.jsx
@@ -1,8 +1,11 @@
 import teams from "../mocks/leaderboard.json";
 import { getRankColor } from "../utils/helpers.js";
+import serieATeams from "../mocks/serieA_standing.json";
+import topScorer from "../mocks/top_scorer.json";
+import topAssist from "../mocks/top_assist.json";
 
 export default function Leaderboard() {
-  const maxPt = Math.max(...teams.map(t => t.total_points))
+  const maxPt = Math.max(...teams.map((t) => t.total_points));
 
   return (
     <div className="flex mx-8 gap-12">
@@ -39,15 +42,21 @@ export default function Leaderboard() {
                     </span>
                   </td>{" "}
                   <td className="p-2 text-center font-bold">{team.team_id}</td>
-                  <td className="p-2 text-center text-gray-500">{team.played}</td>
+                  <td className="p-2 text-center text-gray-500">
+                    {team.played}
+                  </td>
                   <td className="p-2 text-center text-gray-500">{team.won}</td>
-                  <td className="p-2 text-center text-gray-500">{team.drawn}</td>
+                  <td className="p-2 text-center text-gray-500">
+                    {team.drawn}
+                  </td>
                   <td className="p-2 text-center text-gray-500">{team.lost}</td>
                   <td className="p-2 text-center text-gray-500">
                     {team.total_score_for}:{team.total_score_against}
                   </td>
                   <td className="p-2 text-center font-bold">{team.points}</td>
-                  <td className={`p-2 text-center font-semibold ${team.total_points === maxPt && "text-blue-500"}`}>
+                  <td
+                    className={`p-2 text-center font-semibold ${team.total_points === maxPt && "text-blue-500 font-bold!"}`}
+                  >
                     {team.total_points}
                   </td>
                 </tr>
@@ -62,39 +71,85 @@ export default function Leaderboard() {
         </h3>
         <div>
           <h4 className="text-white text-xl font-bold">Classifica Serie A</h4>
-          <table className="text-white">
-            <thead className="text-lg font-bold">
-              <tr>
-                <th className="p-4">Pos.</th>
-                <th className="p-4">Squadra</th>
-                <th className="p-4">Punti</th>
-              </tr>
-            </thead>
-          </table>
+          <div className="rounded-2xl border-4 border-gray-900 overflow-hidden max-h-[22.5vh] overflow-y-auto custom-scrollbar">
+            <table className="w-full rounded-2xl border-collapse">
+              <thead className="font-bold bg-blue-400 top-0 sticky">
+                <tr>
+                  <th className="p-2">Pos.</th>
+                  <th className="p-2">Squadra</th>
+                  <th className="p-2">PG</th>
+                  <th className="p-2">GLS</th>
+                  <th className="p-2">Punti</th>
+                </tr>
+              </thead>
+              <tbody className="bg-gray-50 text-sm">
+                {serieATeams.map((team) => (
+                  <tr
+                    key={team.id}
+                    className="border-b border-gray-500 bg hover:bg-gray-200"
+                  >
+                    <td className="p-2 text-center font-bold">{team.id}</td>
+                    <td className="p-2 text-center font-bold">{team.team_name}</td>
+                    <td className="p-2 text-center text-gray-600">{team.played}</td>
+                    <td className="p-2 text-center text-gray-600">{team.goals_for}:{team.goals_against}</td>
+                    <td className="p-2 text-center font-bold">{team.points}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
         <div>
           <h4 className="text-white text-xl font-bold">Classifica Marcatori</h4>
-          <table className="text-white">
-            <thead className="text-lg font-bold">
-              <tr>
-                <th className="p-4">Gioc.</th>
-                <th className="p-4">Squadra</th>
-                <th className="p-4">#Gol</th>
-              </tr>
-            </thead>
-          </table>
+          <div className="rounded-2xl border-4 border-gray-900 overflow-hidden max-h-[22.5vh] overflow-y-auto custom-scrollbar">
+            <table className="w-full rounded-2xl border-collapse">
+              <thead className="font-bold bg-blue-400 top-0 sticky">
+                <tr>
+                  <th className="p-2">Gioc.</th>
+                  <th className="p-2">Squadra</th>
+                  <th className="p-2">Gol</th>
+                </tr>
+              </thead>
+              <tbody className="bg-gray-50 text-sm">
+                {topScorer.map((team) => (
+                  <tr
+                    key={team.id}
+                    className="border-b border-gray-500 bg hover:bg-gray-200"
+                  >
+                    <td className="p-2 text-center font-bold">{team.player_name}</td>
+                    <td className="p-2 text-center font-bold">{team.team_name}</td>
+                    <td className="p-2 text-center font-bold">{team.goals}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
         <div>
-          <h4 className="text-white text-xl font-bold">Classifica Assistman</h4>
-          <table className="text-white">
-            <thead className="text-lg font-bold">
-              <tr>
-                <th className="p-4">Gioc.</th>
-                <th className="p-4">Squadra</th>
-                <th className="p-4">#Assist</th>
-              </tr>
-            </thead>
-          </table>
+          <h4 className="text-white text-xl font-bold">Classifica Assist</h4>
+          <div className="rounded-2xl border-4 border-gray-900 overflow-hidden max-h-[22.5vh] overflow-y-auto custom-scrollbar">
+            <table className="w-full rounded-2xl border-collapse">
+              <thead className="font-bold bg-blue-400 top-0 sticky">
+                <tr>
+                  <th className="p-2">Gioc.</th>
+                  <th className="p-2">Squadra</th>
+                  <th className="p-2">Assist</th>
+                </tr>
+              </thead>
+              <tbody className="bg-gray-50 text-sm">
+                {topAssist.map((team) => (
+                  <tr
+                    key={team.id}
+                    className="border-b border-gray-500 bg hover:bg-gray-200"
+                  >
+                    <td className="p-2 text-center font-bold">{team.player_name}</td>
+                    <td className="p-2 text-center font-bold">{team.team_name}</td>
+                    <td className="p-2 text-center font-bold">{team.assists}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </div>
